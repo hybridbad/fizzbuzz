9573e7b8532bb799bc39454fbd997b9751ee9272
hybridbad <hybridbad90@gmail.com>
2019-02-26 11:18:13 +0000

    added updated rspec with new tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,31 @@
-require 'fizzbuzz'
+require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end 
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed multiple 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end 
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end 
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+end 
+
+describe 'fizzbuzz' do
+  it 'returns the number if not a multiple of 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end 
